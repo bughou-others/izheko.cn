7aa7e828fcef633989224554f3445f12fbdd3cab
bughou <bughou@gmail.com>
2013-06-18 22:04:32 +0800

    go

--- web/controller/error_page.controller.php
+++ web/controller/error_page.controller.php
@@ -4,13 +4,13 @@ class ErrorPage
 {
     static function gen_404()
     {
-        #ob_start();
+        ob_start();
         require_once APP_ROOT . '/view/layout.view.php';
-#        $path = $_SERVER['REQUEST_DOCUMENT'];
-#        $full_path = $_SERVER['DOCUMENT_ROOT'] . $path;
-#        if(!is_dir($dir = dirname($full_path))) mkdir($dir, 0755, true);
-#        file_put_contents($full_path, ob_get_clean());
-#        header("X-Accel-Redirect: $path");
+        $path = $_SERVER['REQUEST_DOCUMENT'];
+        $full_path = $_SERVER['DOCUMENT_ROOT'] . $path;
+        if(!is_dir($dir = dirname($full_path))) mkdir($dir, 0755, true);
+        file_put_contents($full_path, ob_get_clean());
+        header("X-Accel-Redirect: $path");
     }
 }
 
--- web/controller/item_list.controller.php
+++ web/controller/item_list.controller.php
@@ -29,16 +29,14 @@ class ItemListController
             }
             $page_url = $type && $type !== 'all' ? "/$type/" : '/';
         }
-        #ob_start();
+        ob_start();
         $target_view = 'item_list';
         require_once APP_ROOT . '/view/layout.view.php';
-        /*
         $path = $_SERVER['REQUEST_DOCUMENT'];
         $full_path = $_SERVER['DOCUMENT_ROOT'] . $path;
         if(!is_dir($dir = dirname($full_path))) mkdir($dir, 0755, true);
         file_put_contents($full_path, ob_get_clean());
         header("X-Accel-Redirect: $path");
-         */
     }
 }
 
