1ee6db7e6a3d872ba896a6ff781f71c09e09c753
bughou <bughou@gmail.com>
2013-06-20 14:16:22 +0800

    go

--- www/view/layout.view.php
+++ www/view/layout.view.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <title>爱折扣-精选优质折扣商品</title>
+        <meta name="keywords" content="爱折扣,九块九,九块九包邮,9.9包邮,优质折扣" />
+        <meta name="description" content="爱折扣为您精选了淘宝、 天猫的优质折扣商品， 让您轻松找到物美价廉、 称心如意的宝贝。" />
         <link charset="utf-8" rel="stylesheet" type="text/css" href="/static/main.css" />
     </head>
     <body>
