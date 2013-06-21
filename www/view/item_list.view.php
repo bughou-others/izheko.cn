adc6b96b0e7618aef6c49d93daec18296a8406b8
bughou <bughou@gmail.com>
2013-06-21 20:29:46 +0800

    go

--- www/view/item_list.view.php
+++ www/view/item_list.view.php
@@ -14,15 +14,15 @@
                     <img src="<?= $item->pic_url() ?>" />
                 </a>
                 <div class="buy">
-                    <a class="action <?= $item->action_style() ?>" title="<?= $item->action_title() ?>" href="<?= $item->jump_url() ?>" target="_blank">
-                        <?= $item->action() ?>
-                    </a>
                     <span title="折扣价 ￥<?= $item->discount_price_str() ?>">
                         ￥<big><?= $item->discount_price_yuan() ?></big>.<?= $item->discount_price_fen() ?>
                     </span>
                     <?php if ($item->original_price_str()) { ?>
                     <small title="原价 ￥<?= $item->original_price_str() ?>">￥<?= $item->original_price_str() ?></small>
                     <?php } ?>
+                    <a class="action <?= $item->action_style() ?>" title="<?= $item->action_title() ?>" href="<?= $item->jump_url() ?>" target="_blank">
+                        <?= $item->action() ?>
+                    </a>
                 </div>
                 <div class="flags"><?= $item->postage_tag() ?><?= $item->vip_tag() ?></div>
             </div><!--
