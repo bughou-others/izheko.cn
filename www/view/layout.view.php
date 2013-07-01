add0d4c5068433c7576d227b97bfca0394f2ba64
bughou <bughou@gmail.com>
2013-07-01 18:04:35 +0800

    add my history

--- www/view/layout.view.php
+++ www/view/layout.view.php
@@ -10,9 +10,28 @@
     </head>
     <body>
         <div class="topbar">
-            <span>亲，欢迎来到爱折扣！</span>
+            <span class="left">亲，欢迎来到爱折扣！</span>
             <span class="right">
-                <a href="javascript: void(0);">浏览记录<b></b></a>
+                <span class="my-history">
+                    <a class="my-history" href="/my-history">浏览记录<b></b></a>
+                    <div id="my-history">
+                        <div class="my-history-item">
+                            <a class="image"><img src="http://img04.taobaocdn.com/bao/uploaded/i4/19429035678996345/T1lR9GXtpiXXXXXXXX_!!0-item_pic.jpg_210x210.jpg" /></a>
+                            <span class="desc">
+                                <a>正品意大利STREP诗碧脱毛膏永久绝毛液脱毛膏 包邮</a>
+                                <b>￥19.97</b>
+                            </span>
+                        </div>
+                        <div class="toolbar">
+                            <a href="/my-history">查看全部</a>
+                            <span>清空</span>
+                            <div>
+                                <span>上 ↑</span>
+                                <span>下 ↓</span>
+                            </div>
+                        </div>
+                    </div>
+                </span>
 <!--
                 <a href="javascript: void(0);">手机版</a>
                 <a href="javascript: void(0);">android客户端</a>
