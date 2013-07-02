e6f1b253d74d4e3e10ed3481adf0651dac265094
bughou <bughou@gmail.com>
2013-07-03 00:40:16 +0800

    go

--- static/public/main.js
+++ static/public/main.js
@@ -28,22 +28,22 @@ var MyHistory = {
             var m;
             if(m = document.cookie.match(/(^| )my-history=(\d+(,\d+)*)(;|$)/)) {
                 o.items = m[2].split(',');
+                o.page = 0;
             } else {
                 o.set();
                 return;
             }
         }
 
-        var page_size = 6;
-        if(o.page === undefined) o.page = 0;
+        var page_size = 4;
         if(flag === 'next') o.page ++;
         else if(flag === 'prev') o.page --;
         var max_page = Math.ceil(o.items.length / page_size) - 1;
         if(o.page < 0) o.page = 0;
         else if(o.page > max_page) o.page = max_page;
 
-        //$('#my-history-bar > div > span:first-child').css('visibility', page > 0        ? 'visible' : 'hidden');
-        //$('#my-history-bar > div > span:last-child').css('visibility', page < max_page ? 'visible' : 'hidden');
+        $('#my-history-bar > div > span:first-child').css('visibility', o.page > 0        ? 'visible' : 'hidden');
+        $('#my-history-bar > div > span:last-child') .css('visibility', o.page < max_page ? 'visible' : 'hidden');
 
         var begin = o.page * page_size;
         var end   = begin + page_size;
@@ -73,16 +73,19 @@ var MyHistory = {
     },
     set: function (item_ids) {
         var html;
+        var count = 0;
         if(item_ids) {
             html = '';
             var items_data = this.items_data;
             for(var i = 0; i < item_ids.length; i++) {
                 var id   = item_ids[i];
-                var item = items_data[id] || {  };
+                var item = items_data[id];
+                if(item === undefined) continue;
                 html += '<div class="my-history-item" item-id="' + id + 
                     '"><a class="image" href="' + item.jump_url + '"><img src="' + item.pic_url + 
                     '" /></a><span class="desc"><a href="' + item.jump_url + '">' + item.title + 
                     '</a><b>￥' + item.now_price + '</b></span></div>';
+                count ++;
             }
             $('#my-history-bar').css('display', 'block');
         } else {
@@ -90,6 +93,15 @@ var MyHistory = {
             $('#my-history-bar').css('display', 'none');
         }
         $('#my-history > .my-history-item, #my-history > center').remove();
+        if(this.page === 0 && count > 0) {
+            $('#my-history').css({
+                width : count > 1 ? '464px' : '232px',
+                height: count > 2 ? '262px' : '150px',
+            });
+            $('#my-history-bar').css({
+                width : count > 1 ? '424px' : '192px',
+            });
+        }
         $('#my-history').prepend(html);
     },
 };
