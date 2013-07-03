a37049b7db9be370adb02135c83277372df69d55
bughou <bughou@gmail.com>
2013-07-03 18:06:22 +0800

    go

--- www/view/component/topbar.view.php
+++ www/view/component/topbar.view.php
@@ -2,10 +2,9 @@
             <span class="left">亲，欢迎来到爱折扣！</span>
             <span class="right">
                 <span id="footprints-span">
-                    <a id="footprints-a" href="/footprints">我的足迹<b></b></a>
+                    <div id="footprints-a">我的足迹<b></b></div>
                     <div id="footprints">
                         <div id="footprints-bar">
-                            <a href="/footprints">查看全部</a>
                             <span>清空</span>
                             <div>
                                 <span>上 ↑</span>
