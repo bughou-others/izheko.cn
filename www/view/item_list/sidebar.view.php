6d35f5692a9d8d6f1ada1afe69a1e69574c8a8db
bughou <bughou@gmail.com>
2013-07-04 15:37:35 +0800

    go

--- www/view/item_list/sidebar.view.php
+++ www/view/item_list/sidebar.view.php
@@ -1,7 +1,9 @@
         <div id="sidebar">
-            <div id="go_top"><a href="#"><span>回到顶部</span><b></b></a></div>
+            <div id="go_top"><a href="#"><span>回顶部</span><b></b></a></div>
             <script>
-                Footprints.init();
+                $(window).scroll(function(){
+                    $('#go_top').css('display', $(this).scrollTop() > 100 ? 'block' : 'none');
+                }).scroll();
             </script>
 <?php if($page >= 2) { ?>
             <div id="prev_page"><a href="<?= $page_url . ($page - 1) . '.html' ?>"><span>上一页</span><b></b></a></div>
