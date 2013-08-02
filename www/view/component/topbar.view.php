3131990b6c79e7339395a73289dae94e0aee2b3f
bughou <bughou@gmail.com>
2013-08-02 20:30:11 +0800

    get topbar back

--- www/view/component/topbar.view.php
+++ www/view/component/topbar.view.php
@@ -1,17 +1,4 @@
-            <span id="welcome">
-                <span>亲，欢迎您！</span>
-                <span id="bookmark" title="亲，请按 Ctrl+D 哦"><b></b><span>Ctrl+D</span>收藏爱折扣</span>
-                <script>
-                    if(document.all) { //IE
-                        $('#bookmark').attr('title', '').css('cursor', 'pointer').click(function(){
-                            var url   = 'http://' + location.hostname +'/';
-                            var title = '爱折扣 - 精选优质折扣商品';
-                            window.external.AddFavorite(url, title);
-                        });
-                        $('#bookmark span').text('');
-                    }
-                </script>
-            </span>
+        <div id="topbar">
             <span id="footprints-span">
                 <div id="footprints-a">我的足迹<b></b></div>
                 <div id="footprints">
@@ -27,3 +14,16 @@
             <script>
                 Footprints.init();
             </script>
+            <span id="hello">亲，欢迎您！</span>
+            <span id="bookmark" title="亲，请按 Ctrl+D 哦"><b></b><span>Ctrl+D</span>收藏爱折扣</span>
+            <script>
+                if(document.all) { //IE
+                    $('#bookmark').attr('title', '').css('cursor', 'pointer').click(function(){
+                        var url   = 'http://' + location.hostname +'/';
+                        var title = '爱折扣 - 精选优质折扣商品';
+                        window.external.AddFavorite(url, title);
+                    });
+                    $('#bookmark span').text('');
+                }
+            </script>
+        </div>
