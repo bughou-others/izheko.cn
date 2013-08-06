005d5f91145ded7eea4261b2ab359db65e546971
bughou <bughou@gmail.com>
2013-08-06 14:14:19 +0800

    go

--- www/view/module/header.view.php
+++ www/view/module/header.view.php
@@ -1,10 +1,12 @@
         <div id="topbar">
 <?php
     require APP_ROOT . "/view/component/footprint.view.php";
-    require APP_ROOT . "/view/component/bookmark.view.php";
-    require APP_ROOT . "/view/component/sns_share.view.php";
 ?>
             <span id="hello">亲，欢迎您！</span>
+<?php
+    require APP_ROOT . "/view/component/sns_share.view.php";
+    require APP_ROOT . "/view/component/bookmark.view.php";
+?>
         </div>
         <div id="header">
             <a href="/" class="logo"></a>
