e83171d907141d0b9e97439ebc14b5c8bc7288f6
bughou <bughou@gmail.com>
2013-08-13 17:37:54 +0800

    go

--- www/view/module/footer.view.php
+++ www/view/module/footer.view.php
@@ -1,8 +1,12 @@
         <div id="footer">
-                亲，还在淘宝大海捞针？ 爱折扣精选了淘宝、 天猫的优质折扣商品， 您俯拾即是。<br />
-                爱折扣仅提供到淘宝、天猫的宝贝链接， 所有交易都在淘宝、天猫进行， 请放心购买。<br />
-                 <div>
-                    Copyright©2013 izheko.cn 爱折扣 版权所有 <br />
-                    <small>蜀ICP备09019425号 <script src="http://s25.cnzz.com/stat.php?id=5503631&web_id=5503631" language="JavaScript"></script></small>
-                 </div>
+            <a>常见问题</a>
+            <a>交易安全</a>
+            <a>关于我们</a>
+            <a>联系我们</a>
+            <div>
+                <span>Copyright © 2013 izheko.cn</span>
+                <span>爱折扣 版权所有</span>
+                <small>蜀ICP备09019425号</small>
+                <sub><script src="http://s25.cnzz.com/stat.php?id=5503631&web_id=5503631"></script></sub>
+            </div>
         </div>
