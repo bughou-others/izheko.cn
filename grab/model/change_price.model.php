119820d7e9cefb8367d2bfbaef5624076385f0f7
bughou <bughou@gmail.com>
2013-08-26 17:58:14 +0800

    change price rules

--- grab/model/change_price.model.php
+++ grab/model/change_price.model.php
@@ -6,14 +6,17 @@ class ChangePrice
     static function parse($str)
     {
         //var_dump($str);
-        $change_price = '(?:(?:[改变减降]价?|修改)[成为至到]?)';
+        $change_price = '(?:修?[改变]价?[成为至到]?|[减降]价?[成为至到]|只要)';
         $format_array = array(
-            "拍下?后?就?(?:$change_price|只要)?%s", "自动$change_price?%s", '^[促改]?%s$', '秒杀价?%s'
+            "拍下?后?就?立?$change_price?%s",
+            "自动$change_price?%s", '^[促改]?%s$',
+            '(?:秒杀价?|实付|特惠|价格为)%s',
+            '%s包邮',
         );
         foreach($format_array as $format)
         {
             $re = '/' . sprintf($format,
-                '￥?([0-9一二三四五六七八九十]{1,3})[元块点]([0-9零一二三四五六七八九]{1,2})?')
+                ' *￥?([0-9一二三四五六七八九十]{1,3})[元块点]([0-9零一二三四五六七八九]{1,2})?元?')
                 . '/u';
             if (preg_match($re, $str, $m))
             {
@@ -26,7 +29,7 @@ class ChangePrice
             }
             else
             {
-                $re = '/' . sprintf($format, '￥?([0-9]{1,3}(\.[0-9]{1,2})?)[元块]?') . '/u';
+                $re = '/' . sprintf($format, ' *￥?([0-9]{1,3}(\.[0-9]{1,2})?)[元块]?') . '/u';
                 if (preg_match($re, $str, $m))
                 {
                     return array('price' => $m[1], 'price_type' => '拍下改价');
