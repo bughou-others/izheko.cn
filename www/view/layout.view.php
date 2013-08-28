13798edc22cefd634987fe61eaff61f223dd7a02
bughou <bughou@gmail.com>
2013-08-28 19:48:20 +0800

    go

--- www/view/layout.view.php
+++ www/view/layout.view.php
@@ -22,7 +22,7 @@
         <link rel="shortcut icon" href="<?= App::static_server() . "/img/favicon$icon_suffix.ico" ?>?v=20130817" />
         <link charset="utf-8" rel="stylesheet" type="text/css" href="<?= App::static_server(). "/css/main$suffix.css" ?>?v=20130817" />
         <script src="<?= App::static_server() ?>/js/jquery.min.js"></script>
-        <script src="<?= App::static_server() ?>/js/main.js?v=20130817"></script>
+        <script src="<?= App::static_server() ?>/js/main.js?v=20130828"></script>
     </head>
     <body>
         <?php require APP_ROOT . "/view/module/header$header_suffix.view.php"; ?>
