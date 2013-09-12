8099e8adcfd33084ada3f5154b2f5ffe3b7aaac7
bughou <bughou@gmail.com>
2013-09-12 14:45:49 +0800

    go

--- www/view/module/sidebar.view.php
+++ www/view/module/sidebar.view.php
@@ -5,15 +5,5 @@
                     $('#go_top').css('display', $(this).scrollTop() > 100 ? 'block' : 'none');
                 }).scroll();
             </script>
-<?php
-if(isset($page) && $page >= 2) {
-?>
-            <div id="prev_page"><a href="<?= $page_url . ($page - 1) . '.html' ?>"><span>上一页</span><b></b></a></div>
-<?php
-} 
-if(isset($data['total_count'], $page_size, $page) && ($data['total_count'] / $page_size) > $page) { 
-?>
-            <div id="next_page"><a href="<?= $page_url . ($page + 1) . '.html' ?>"><span>下一页</span><b></b></a></div>
-<?php } ?>
             <div id="kefu"><a href="tencent://message/?uin=715091790"><span>客 服</span><b></b></a></div>
         </div>
