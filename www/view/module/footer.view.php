3326c94d2b57c80217a83d42a74fc2af0cd1601a
bughou <bughou@gmail.com>
2013-09-28 22:52:32 +0800

    go

--- www/view/module/footer.view.php
+++ www/view/module/footer.view.php
@@ -1,14 +1,14 @@
         <div id="footer">
             <span>
-                <a href="/help.html?#faq">常见问题</a>
-                <a href="/help.html?#trans">交易安全</a>
-                <a href="/help.html?#phone">手机版</a>
+                <a href="/help.html?#newbile">新手上路</a>
+                <a href="/help.html?#seckill">秒杀秘笈</a>
+                <a href="/help.html?#phone">手机版爱折扣</a>
             </span>
             <a href="/" class="footer-logo"></a>
             <span>
-                <a href="/help.html?#about">关于我们</a>
-                <a href="/help.html?#contact">联系我们</a>
-                <a href="tencent://message/?uin=715091790">在线客服</a>
+                <a href="/help.html?#faq">常见问题</a>
+                <a href="/help.html?#about">关于爱折扣</a>
+                <a href="tencent://message/?uin=715091790">意见和建议</a>
             </span>
             <div>
                 <span>Copyright © 2013 izheko.cn</span>
--- www/view/module/help.view.php
+++ www/view/module/help.view.php
@@ -1,63 +1,103 @@
 <style>
-#help-wrapper {
-    margin: 20px 5%;
-    padding: 2em 2em 3em 2em;
-    text-align: left;
-    background-color: #fff;
-}
-#help-wrapper h2 {
-    color: #390;
-}
-#help-wrapper dt, #help-wrapper dd {
-    margin: 10px;
-    font: normal 14px/22px "宋体";
-}
-#help-wrapper dt span { 
-    color: #5aae22;
-}
-#help-wrapper dd {
-    color: #af7105;
-}
-#help-wrapper dt span, #help-wrapper dd span {
-    margin-right: 6px;
-    font: normal 16px/22px "宋体";
-}
-#help-wrapper p {
-    margin: 10px;
-    font: normal 14px/22px "宋体";
-}
+    #help-wrapper {
+        position: relative;
+        margin: 20px 5%;
+        text-align: left;
+    }
+    #help-catalog {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 200px;
+        padding: 20px;
+        line-height: 24px;
+        background-color: #fff;
+    }
+    #help-catalog div {
+        margin-bottom: 20px;
+    }
+    #help-catalog span {
+        font-size: 14px;
+    }
+    #help-catalog a {
+        display: block;
+    }
+
+    #help-content {
+        margin-left: 260px;
+        padding: 20px;
+        background-color: #fff;
+    }
+    #help-content h2 {
+        color: #390;
+    }
+    #help-content dt, #help-content dd {
+        margin: 10px;
+        font: normal 14px/22px "宋体";
+    }
+    #help-content dt span { 
+        color: #5aae22;
+    }
+    #help-content dd {
+        color: #af7105;
+    }
+    #help-content dt span, #help-content dd span {
+        margin-right: 6px;
+        font: normal 16px/22px "宋体";
+    }
+    #help-content p {
+        margin: 10px;
+        font: normal 14px/22px "宋体";
+    }
 </style>
-
 <div id="help-wrapper">
-<a name="faq"></a>
-<h2>常见问题</h2>
-<dt><span>问题:</span> 什么是VIP价？</dt>
-<dd><span>回答:</span> VIP价格是淘宝卖家设定的一种促销方式，只有淘宝VIP账号才能享受到优惠价，VIP账号登录后直接拍下即可。</dd>
-<dt><span>问题:</span> 如何查看我的淘宝账号是否是VIP？</dt>
-<dd><span>回答:</span> 登陆淘宝，在淘宝会员页面（http://vip.taobao.com/growth_info.htm）查看会员等级。V1-V6可以享受VIP价。</dd>
-<dt><span>问题:</span> 如果还不是VIP，怎么买VIP价商品？</dt>
-<dd><span>回答:</span> 如果不是淘宝VIP账号，需要联系卖家，按照卖家的指示进行购买才能享受VIP价。</dd>
+    <div id="help-catalog">
+        <div>
+            <span>玩转爱折扣</span>
+            <a href="#newbile">新手上路</a>
+            <a href="#seckill">秒杀秘笈</a>
+            <a href="#phone">手机版爱折扣</a>
+        </div>
+        <div>
+            <span>常见问题</span>
+            <a href="#faq-vip">什么是VIP价</a>
+            <a href="#faq-safe">在爱折扣购买商品，安全吗</a>
+        </div>
+        <div>
+            <span>认识爱折扣</span>
+            <a href="#about">爱折扣 - 精选优质折扣商品</a>
+            <a href="tencent://message/?uin=715091790">爱折扣 - 意见和建议</a>
+            爱折扣用户交流QQ群：72250699
+        </div>
+    </div>
 
-<a name="trans"></a>
-<h2>交易安全</h2>
-<p>爱折扣仅提供到淘宝、天猫的宝贝链接，不涉及任何交易行为，所有交易都在淘宝、天猫进行，所以不存在交易安全问题，请放心购买。</p>
+    <div id="help-content">
+        <a name="phone"></a>
+        <h2>手机版爱折扣</h2>
+        <p>爱折扣在电脑、手机、平板等设备上都表现完美。只需要用您喜欢的浏览器打开www.izheko.cn，无论是在电脑、平板，还是手机上，您都会获得一致的用户体验。</p>
 
-<a name="phone"></a>
-<h2>手机版</h2>
-<p>爱折扣在电脑、手机、平板等设备上都表现完美。只需要用您喜欢的浏览器打开www.izheko.cn，无论是在电脑、平板，还是手机上，您都会获得一致的用户体验。</p>
+        <a name="faq"></a>
+        <h2>常见问题</h2>
+        <a name="faq-vip"></a>
+        <dt><span>问题:</span> 什么是VIP价？</dt>
+        <dd><span>回答:</span> VIP价格是淘宝卖家设定的一种促销方式，只有淘宝VIP账号才能享受到优惠价，VIP账号登录后直接拍下即可。</dd>
+        <dt><span>问题:</span> 如何查看我的淘宝账号是否是VIP？</dt>
+        <dd><span>回答:</span> 登陆淘宝，在淘宝会员页面（http://vip.taobao.com/growth_info.htm）查看会员等级。V1-V6可以享受VIP价。</dd>
+        <dt><span>问题:</span> 如果还不是VIP，怎么买VIP价的商品？</dt>
+        <dd><span>回答:</span> 如果不是淘宝VIP账号，需要联系卖家，按照卖家的指示进行购买才能享受VIP价。</dd>
 
-<a name="about"></a>
-<h2>爱折扣 - 精选优质折扣商品</h2>
-<p>在网购繁荣的今天，要淘到一件自己满意的宝贝却并不容易。淘宝的在线商品数量已超过8亿，从这么多质量和价格都参差不齐的商品中，挑选出一件让自己满意的商品，往往需要花费几十分钟，甚至几个小时。</p>
-<p>在制造过剩的今天，我们不仅要买到优质的商品，同时还要享受到优惠的价格，可常常都还是觉得“这东西好贵”；另一方面，每天都有大量的商品在打折促销，其中不乏物美价廉的优质精品；为了吸引用户、提高信誉、增加流量，很多卖家甚至以低于成本的价格在进行促销。</p>
-<p>爱折扣从这些正在打折促销的淘宝、 天猫商品中，精选出价值几十甚至上百块，折扣价只要9块9或更低，还包邮的优质商品，让用户得到最大的实惠，大大缩短用户挑选商品的时间，同时享受到最优惠的折扣价。目前爱折扣每天都有数千款9块9包邮商品，包括各种女装、男装、居家用品、母亲婴儿用品、鞋子、包包、配件、饰品、美食、数码用品、美容用品、化妆用品、文化体育用品、汽车用品等。爱折扣，让你轻松淘到物美价廉的宝贝。</p>
-<p>亲，还在淘宝大海捞针？还在跟卖家砍价砍运费？不必了！在爱折扣，足够好的商品，足够低的价格，您俯拾即是。</p>
-<p>爱生活，爱折扣；最优质的商品，最给力的折扣价！</p>
-<p>每天来逛逛，收获您的小幸福。</p>
+        <a name="faq-safe"></a>
+        <dt><span>问题:</span> 在爱折扣购买商品，安全吗？</dt>
+        <dd><span>回答:</span> 爱折扣仅提供到淘宝、天猫的商品链接，不涉及任何交易行为，所有交易都在淘宝、天猫进行，所以爱折扣不存在交易安全问题，请放心购买。</dd>
 
-<a name="contact"></a>
-<h2>联系我们</h2>
-<p>地址：四川省 成都市 高新区 天府大道南段651号 怡丰新城 10栋3单元612</p>
-<p>邮编：610213</p>
-<p>爱折扣 用户交流QQ群：72250699</p>
+        <a name="about"></a>
+        <h2>爱折扣 - 精选优质折扣商品</h2>
+        <p>在网购繁荣的今天，要淘到一件自己满意的宝贝却并不容易。淘宝的在线商品数量已超过8亿，从这么多质量和价格都参差不齐的商品中，挑选出一件让自己满意的商品，往往需要花费几十分钟，甚至几个小时。</p>
+        <p>在制造过剩的今天，我们不仅要买到优质的商品，同时还要享受到优惠的价格，可常常都还是觉得“这东西好贵”；另一方面，每天都有大量的商品在打折促销，其中不乏物美价廉的优质精品；为了吸引用户、提高信誉、增加流量，很多卖家甚至以低于成本的价格在进行促销。</p>
+        <p>爱折扣从这些正在打折促销的淘宝、 天猫商品中，精选出价值几十甚至上百块，折扣价只要9块9或更低，还包邮的优质商品，让用户得到最大的实惠，大大缩短用户挑选商品的时间，同时享受到最优惠的折扣价。目前爱折扣每天都有数千款9块9包邮商品，包括各种女装、男装、居家用品、母亲婴儿用品、鞋子、包包、配件、饰品、美食、数码用品、美容用品、化妆用品、文化体育用品、汽车用品等。爱折扣，让你轻松淘到物美价廉的宝贝。</p>
+        <p>亲，还在淘宝大海捞针？还在跟卖家砍价砍运费？不必了！在爱折扣，足够好的商品，足够低的价格，您俯拾即是。</p>
+        <p>爱生活，爱折扣；最优质的商品，最给力的折扣价！</p>
+        <p>每天来逛逛，收获您的小幸福。</p>
+
+    </div>
 </div>
