d1ee3d1f476c5ee6ad73662ccef4b20046bfd345
bughou <bughou@gmail.com>
2013-09-30 20:23:10 +0800

    go

--- www/view/module/help.view.php
+++ www/view/module/help.view.php
@@ -2,10 +2,10 @@
 <div id="help-wrapper">
     <span id="help-catalog">
         <div>
-            <span>认识爱折扣</span>
-            <a href="#about">精选优质折扣商品</a>
-            <a href="tencent://message/?uin=715091790">意见和建议</a>
-            用户交流QQ群：<b>72250699</b>
+            <span>玩转爱折扣</span>
+            <a href="#newbile">爱折扣 - 新手上路</a>
+            <a href="#seckill">爱折扣 - 秒杀秘笈</a>
+            <a href="#phone">爱折扣 - 手机版</a>
         </div>
         <div>
             <span>常见问题</span>
@@ -13,10 +13,10 @@
             <a href="#faq-safe">在爱折扣购买商品，安全吗</a>
         </div>
         <div>
-            <span>玩转爱折扣</span>
-            <a href="#phone">爱折扣 - 手机版</a>
-            <a href="#seckill">爱折扣 - 秒杀秘笈</a>
-            <a href="#newbile">爱折扣 - 新手上路</a>
+            <span>关于爱折扣</span>
+            <a href="#about">精选优质折扣商品</a>
+            <a href="tencent://message/?uin=715091790">意见和建议</a>
+            用户交流QQ群：<b>72250699</b>
         </div>
     </span>
     <script>
@@ -26,94 +26,19 @@
             var $catalog = $('#help-catalog');
             $window.bind('scroll resize', function(){
                 if ($catalog.css('position') !== 'static') {
-                    if ($window.scrollTop() < $wrapper.offset().top) {
-                        $catalog.removeClass('fixed bottom');
-                    } else if ($window.scrollTop() + $catalog.height()
-                        < $wrapper.offset().top + $wrapper.height()
-                    ) {
-                        $catalog.removeClass('bottom').addClass('fixed');
-                    } else {
-                        $catalog.removeClass('fixed').addClass('bottom');
+                    var _top = $window.scrollTop() - $wrapper.offset().top;
+                    if (_top <= 0) _top = 0;
+                    else {
+                        var max_top = $wrapper.height() - $catalog.height();
+                        if (_top > max_top) _top = max_top;
                     }
-                } else $catalog.removeClass('fixed bottom');
+                    $catalog.css('top', _top + 'px');
+                } else $catalog.css('top', '0');
             });
         })();
     </script>
 
     <div id="help-content">
-        <div class="help-section">
-            <a name="about"></a>
-            <h2>爱折扣 - 精选优质折扣商品</h2>
-            <p>在网购繁荣的今天，要淘到一件自己满意的宝贝却并不容易。淘宝的在线商品数量已超过8亿，从这么多质量和价格都参差不齐的商品中，挑选出一件让自己满意的商品，往往需要花费几十分钟，甚至几个小时。</p>
-            <p>在制造过剩的今天，我们不仅要买到优质的商品，同时还要享受到优惠的价格，可常常都还是觉得“这东西好贵”；另一方面，每天都有大量的商品在打折促销，其中不乏物美价廉的优质精品；为了吸引用户、提高信誉、增加流量，很多卖家甚至以低于成本的价格在进行促销。</p>
-            <p>爱折扣从这些正在打折促销的淘宝、 天猫商品中，精选出价值几十甚至上百块，折扣价只要9块9或更低，还包邮的优质商品，让用户得到最大的实惠，大大缩短用户挑选商品的时间，同时享受到最优惠的折扣价。目前爱折扣每天都有数千款9块9包邮商品，包括各种女装、男装、居家用品、母亲婴儿用品、鞋子、包包、配件、饰品、美食、数码用品、美容用品、化妆用品、文化体育用品、汽车用品等。爱折扣，让你轻松淘到物美价廉的宝贝。</p>
-            <p>亲，还在淘宝大海捞针？还在跟卖家砍价砍运费？不必了！在爱折扣，足够好的商品，足够低的价格，您俯拾即是。</p>
-            <p>爱生活，爱折扣；最优质的商品，最给力的折扣价！</p>
-            <p>每天来逛逛，收获您的小幸福。</p>
-        </div>
-
-        <div class="help-section">
-            <a name="faq"></a>
-            <h2>常见问题</h2>
-            <a name="faq-vip"></a>
-            <dt><b>问题:</b> <div>什么是VIP价？</div></dt>
-            <dd><b>回答:</b> <div>VIP价格是淘宝卖家设定的一种促销方式，只有淘宝VIP账号才能享受到优惠价，VIP账号登录后直接拍下即可。</div></dd>
-            <dt><b>问题:</b> <div>如何查看我的淘宝账号是否是VIP？</div></dt>
-            <dd><b>回答:</b> <div>登陆淘宝，在淘宝会员页面（http://vip.taobao.com/growth_info.htm）查看会员等级。V1-V6可以享受VIP价。</div></dd>
-            <dt><b>问题:</b> <div>如果还不是VIP，怎么买VIP价的商品？</div></dt>
-            <dd><b>回答:</b> <div>如果不是淘宝VIP账号，需要联系卖家，按照卖家的指示进行购买才能享受VIP价。</div></dd>
-
-            <a name="faq-safe"></a>
-            <dt><b>问题:</b> <div>在爱折扣购买商品，安全吗？</div></dt>
-            <dd><b>回答:</b> <div>爱折扣仅提供到淘宝、天猫的商品链接，不涉及任何交易行为，所有交易都在淘宝、天猫进行，所以爱折扣不存在交易安全问题，请放心购买。</div></dd>
-        </div>
-
-        <div class="help-section">
-            <a name="phone"></a>
-            <h2>手机版爱折扣</h2>
-            <p>爱折扣在电脑、手机、平板等设备上都表现完美。只需要用您喜欢的浏览器打开www.izheko.cn，无论是在电脑、平板，还是手机上，您都会获得一致的用户体验。</p>
-        </div>
-
-        <div class="help-section">
-            <a name="seckill"></a>
-            <h2>秒杀秘笈</h2>
-            <p>
-            什么是“秒杀"？几十件、几百件宝贝瞬间被一抢而空啊！额滴神，这速度没点技巧怎么行？折800小编传授四大独门秒杀技，教你抢到手抽筋。
-            </p>
-            <div class="seckill">
-                <b>秒杀第一式：</b> <strong>高水平的硬件配置</strong>
-                <p>
-                看看你的电脑配置和网速处在什么水平？不是顶级最起码也要达到中上，遇上死机、打不开页面的话神仙也不能帮你，快，去给电脑升个级先！
-                </p>
-            </div>
-            <div class="seckill">
-                <b>秒杀第二式：</b> <strong>注重软件细节</strong>
-                <p>
-                尝试安装更快速的浏览器，比如Chrome和火狐，提速0.1秒也许你就赢了！
-                </p>
-            </div>
-            <div class="seckill">
-                <b>秒杀第三式：</b> <strong>做好充分准备</strong>
-                <p>
-                一秒钟内的决战绝对需要充分的准备，看准宝贝秒杀开始时间、提前打开宝贝页面等待、确保支付宝中有充裕的余额，最好设个闹钟提醒，还有别忘了提前登录，时间一到迅速刷新
-                <br>
-                <img src="<?= App::static_server() ?>/other/seckill.jpg" />
-                </p>
-            </div>
-            <div class="seckill">
-                <b>秒杀第四式：</b> <strong>别停，立即支付</strong>
-                <p>
-                秒杀与其他活动有所差别，成功与否以支付为准，别想着拍下了就成功了，付款稍慢1秒名额就没了，特别提醒，秒杀前多余地址清掉，否则愣上一愣你就与心爱的宝贝失之交臂
-                </p>
-            </div>
-            <div class="seckill">
-                <b>秒杀最终式：</b> <strong>结束了，淡定</strong>
-                <p>
-                你说我上面的都准备好了，仍然没抢到，那能怎样，淡定呗！接下来还是要不断提高自己的手眼协调能力以及反应能力，秒杀的乐趣是什么？不在于收获的结果，而是享受秒杀过程中的那种精神快感，生活需要激情！
-                </p>
-            </div>
-        </div>
-
         <div class="help-section">
             <a name="newbile"></a>
             <h2>新手上路：如何在爱折扣参与抢购活动</h2>
@@ -158,6 +83,79 @@
                 </span>
                 <img src="<?= App::static_server() ?>/other/yqg.png" />
             </div>
+        </div>
+
+        <div class="help-section">
+            <a name="seckill"></a>
+            <h2>秒杀秘笈</h2>
+            <p>
+            什么是“秒杀"？几十件、几百件宝贝瞬间被一抢而空啊！额滴神，这速度没点技巧怎么行？折800小编传授四大独门秒杀技，教你抢到手抽筋。
+            </p>
+            <div class="seckill">
+                <b>秒杀第一式：</b> <strong>高水平的硬件配置</strong>
+                <p>
+                看看你的电脑配置和网速处在什么水平？不是顶级最起码也要达到中上，遇上死机、打不开页面的话神仙也不能帮你，快，去给电脑升个级先！
+                </p>
+            </div>
+            <div class="seckill">
+                <b>秒杀第二式：</b> <strong>注重软件细节</strong>
+                <p>
+                尝试安装更快速的浏览器，比如Chrome和火狐，提速0.1秒也许你就赢了！
+                </p>
+            </div>
+            <div class="seckill">
+                <b>秒杀第三式：</b> <strong>做好充分准备</strong>
+                <p>
+                一秒钟内的决战绝对需要充分的准备，看准宝贝秒杀开始时间、提前打开宝贝页面等待、确保支付宝中有充裕的余额，最好设个闹钟提醒，还有别忘了提前登录，时间一到迅速刷新
+                <br>
+                <img src="<?= App::static_server() ?>/other/seckill.jpg" />
+                </p>
+            </div>
+            <div class="seckill">
+                <b>秒杀第四式：</b> <strong>别停，立即支付</strong>
+                <p>
+                秒杀与其他活动有所差别，成功与否以支付为准，别想着拍下了就成功了，付款稍慢1秒名额就没了，特别提醒，秒杀前多余地址清掉，否则愣上一愣你就与心爱的宝贝失之交臂
+                </p>
+            </div>
+            <div class="seckill">
+                <b>秒杀最终式：</b> <strong>结束了，淡定</strong>
+                <p>
+                你说我上面的都准备好了，仍然没抢到，那能怎样，淡定呗！接下来还是要不断提高自己的手眼协调能力以及反应能力，秒杀的乐趣是什么？不在于收获的结果，而是享受秒杀过程中的那种精神快感，生活需要激情！
+                </p>
+            </div>
+        </div>
+
+        <div class="help-section">
+            <a name="phone"></a>
+            <h2>手机版爱折扣</h2>
+            <p>爱折扣在电脑、手机、平板等设备上都表现完美。只需要用您喜欢的浏览器打开www.izheko.cn，无论是在电脑、平板，还是手机上，您都会获得一致的用户体验。</p>
+        </div>
+
+        <div class="help-section">
+            <a name="faq"></a>
+            <h2>常见问题</h2>
+            <a name="faq-vip"></a>
+            <dt><b>问题:</b> <div>什么是VIP价？</div></dt>
+            <dd><b>回答:</b> <div>VIP价格是淘宝卖家设定的一种促销方式，只有淘宝VIP账号才能享受到优惠价，VIP账号登录后直接拍下即可。</div></dd>
+            <dt><b>问题:</b> <div>如何查看我的淘宝账号是否是VIP？</div></dt>
+            <dd><b>回答:</b> <div>登陆淘宝，在淘宝会员页面（http://vip.taobao.com/growth_info.htm）查看会员等级。V1-V6可以享受VIP价。</div></dd>
+            <dt><b>问题:</b> <div>如果还不是VIP，怎么买VIP价的商品？</div></dt>
+            <dd><b>回答:</b> <div>如果不是淘宝VIP账号，需要联系卖家，按照卖家的指示进行购买才能享受VIP价。</div></dd>
+
+            <a name="faq-safe"></a>
+            <dt><b>问题:</b> <div>在爱折扣购买商品，安全吗？</div></dt>
+            <dd><b>回答:</b> <div>爱折扣仅提供到淘宝、天猫的商品链接，不涉及任何交易行为，所有交易都在淘宝、天猫进行，所以爱折扣不存在交易安全问题，请放心购买。</div></dd>
+        </div>
+
+        <div class="help-section">
+            <a name="about"></a>
+            <h2>爱折扣 - 精选优质折扣商品</h2>
+            <p>在网购繁荣的今天，要淘到一件自己满意的宝贝却并不容易。淘宝的在线商品数量已超过8亿，从这么多质量和价格都参差不齐的商品中，挑选出一件让自己满意的商品，往往需要花费几十分钟，甚至几个小时。</p>
+            <p>在制造过剩的今天，我们不仅要买到优质的商品，同时还要享受到优惠的价格，可常常都还是觉得“这东西好贵”；另一方面，每天都有大量的商品在打折促销，其中不乏物美价廉的优质精品；为了吸引用户、提高信誉、增加流量，很多卖家甚至以低于成本的价格在进行促销。</p>
+            <p>爱折扣从这些正在打折促销的淘宝、 天猫商品中，精选出价值几十甚至上百块，折扣价只要9块9或更低，还包邮的优质商品，让用户得到最大的实惠，大大缩短用户挑选商品的时间，同时享受到最优惠的折扣价。目前爱折扣每天都有数千款9块9包邮商品，包括各种女装、男装、居家用品、母亲婴儿用品、鞋子、包包、配件、饰品、美食、数码用品、美容用品、化妆用品、文化体育用品、汽车用品等。爱折扣，让你轻松淘到物美价廉的宝贝。</p>
+            <p>亲，还在淘宝大海捞针？还在跟卖家砍价砍运费？不必了！在爱折扣，足够好的商品，足够低的价格，您俯拾即是。</p>
+            <p>爱生活，爱折扣；最优质的商品，最给力的折扣价！</p>
+            <p>每天来逛逛，收获您的小幸福。</p>
         </div>
 
     </div>
