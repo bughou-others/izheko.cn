8973c6f4bc3e1e690d21133ce76e8986e9b2a982
bughou <bughou@gmail.com>
2013-10-24 20:22:20 +0800

    beautify html indent

--- www/view/content/item_list.view.php
+++ www/view/content/item_list.view.php
@@ -8,11 +8,13 @@ if (empty($data['items'])) { ?>
 <?php foreach($data['items'] as $i => $item) { ?>
 <div class="item-wrapper"><div class="item">
     <a class="pic" id="pic<?= ++$i ?>" data-itemid="<?= $item->get('num_iid') ?>" target="_blank" href="#"></a><?php if ($i === 10 || $i === $item_count) { ?><script> Izheko.lazy_img(); </script><?php } ?>
+
     <div class="title"><?= $item->type_tag() ?><a href="#" data-itemid="<?= $item->get('num_iid') ?>" target="_blank"><?= $item->title() ?></a></div>
     <div class="buy">
-        <span title="折扣价 ￥<?= $item->discount_price_str() ?>">￥<big><?= $item->discount_price_yuan() ?></big>.<?= $item->discount_price_fen() ?></span>
-        <?php if ($item->original_price_str()) { ?><small title="原价 ￥<?= $item->original_price_str() ?>">￥<?= $item->original_price_str() ?></small><?php } ?>
+        <span title="折扣价 ￥<?= $item->discount_price_str() ?>">￥<big><?= $item->discount_price_yuan() ?></big>.<?= $item->discount_price_fen() ?></span><?php if ($item->original_price_str()) { ?><small title="原价 ￥<?= $item->original_price_str() ?>">￥<?= $item->original_price_str() ?></small><?php } ?>
+
         <?= $item->postage_tag() ?><?= $item->paigai_tag() ?><?= $item->vip_tag() ?>
+
         <a data-rd="1" class="action <?= $item->action_style() ?>" title="<?= $item->action_title() ?>" href="#" data-itemid="<?= $item->get('num_iid') ?>" target="_blank"><?= $item->action() ?></a>
     </div>
     <div class="expand">
@@ -20,6 +22,7 @@ if (empty($data['items'])) { ?>
         <div class="tip">小编： <span><?= $item->get('ref_tip') ?></span></div>
     </div>
 </div></div>
+
 <?php } ?>
             </div>
             <div id="pagination">
