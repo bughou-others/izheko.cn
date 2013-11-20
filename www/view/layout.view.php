751d9ece5d412bf66e58c13a51ad6e50e0b3e41a
bughou <bughou@gmail.com>
2013-11-20 12:59:56 +0800

    go

--- www/view/layout.view.php
+++ www/view/layout.view.php
@@ -6,7 +6,7 @@
         <meta name="viewport"   content="width=device-width,initial-scale=1.0" />
 <?php require 'module/seo.view.php'; ?>
         <link href="<?= App::static_server() ?>/img/favicon.ico?v=20130817" rel="shortcut icon" />
-        <link href="<?= App::static_server() ?>/css/main.css?v=20131119.1" rel="stylesheet" />
+        <link href="<?= App::static_server() ?>/css/main.css?v=20131120.1" rel="stylesheet" />
         <script src="<?= App::static_server() ?>/js/jquery.min.js"></script>
         <script src="<?= App::static_server() ?>/js/main.js?v=20131119.1"></script>
     </head>
